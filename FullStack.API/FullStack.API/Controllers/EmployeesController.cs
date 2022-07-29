0c0e4dafd3d9da2c8f33cf54d586c910bb404121
Per Rye Hougaard <per.hougaard@3f.dk>
2022-07-29 10:53:18 +0200

    work

---
+++ FullStack.API/FullStack.API/Controllers/EmployeesController.cs
@@ -0,0 +1,35 @@
+﻿using FullStack.API.Data;
+using FullStack.API.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace FullStack.API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class EmployeesController : Controller
+    {
+        private readonly FullStackDbContext _dbContext;
+
+        public EmployeesController(FullStackDbContext fullStackDbContext)
+        {
+            this._dbContext = fullStackDbContext;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAllEmployees()
+        {
+            var employees = await _dbContext.Employees.ToListAsync();
+            return Ok(employees);
+        }
+        
+        [HttpPost]
+        public async Task<IActionResult> AddEmployee(Employee employee)
+        {
+            employee.Id = Guid.NewGuid();
+            await _dbContext.Employees.AddAsync(employee);
+            await _dbContext.SaveChangesAsync();
+            return Ok(employee);
+        }
+    }
+}
